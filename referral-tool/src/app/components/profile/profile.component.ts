7ac813f9930ae28c3acfc686d84c64b715498a4c
Lorenzo Borghs <lorenzo.borghs@gmail.com>
2021-08-08 16:08:20 +0200

    Make it interactive

--- referral-tool/src/app/components/profile/profile.component.ts
+++ referral-tool/src/app/components/profile/profile.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  favorite: boolean = false;
+  profile: any = {
+    name: 'Lorenzo Borghs',
+    birthday: '23 augustus 1992',
+    position: 'Front-End Developer',
+    location: 'Turnhout',
+    yearsExperience: 5,
+    referredBy: 'Joey Comhaire',
+    tags: ['Angular', 'React', 'Javascript'],
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleFavorite(): void {
+    alert('Favorite');
   }
-
 }
