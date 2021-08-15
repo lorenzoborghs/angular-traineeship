6178682950b4418437ce209aa633ddb2c379fa84
Lorenzo Borghs <lorenzo.borghs@gmail.com>
2021-08-15 15:19:16 +0200

    Add functionality to add and remove tags

--- referral-tool/src/app/app.module.ts
+++ referral-tool/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { TagComponent } from './components/tag/tag.component';
 
 @NgModule({
-  declarations: [AppComponent, ProfileComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ProfileComponent, TagComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- referral-tool/src/app/components/profile/profile.component.ts
+++ referral-tool/src/app/components/profile/profile.component.ts
@@ -8,22 +8,34 @@ import { Profile } from 'src/app/interfaces/profile';
 })
 export class ProfileComponent implements OnInit {
   favorite: boolean = false;
-  profile: Profile = {
-    name: 'Lorenzo Borghs',
-    picture: 'https://placebear.com/80/80',
-    birthday: new Date('08/23/1992'),
-    position: 'Front-End Developer',
-    location: 'Turnhout',
-    yearsExperience: 5,
-    referredBy: 'Joey Comhaire',
-    tags: ['Angular', 'React', 'Javascript'],
-  };
+  profile: Profile;
+  newTag: string;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.profile = {
+      name: 'Lorenzo Borghs',
+      picture: 'https://placebear.com/80/80',
+      birthday: new Date('08/23/1992'),
+      position: 'Front-End Developer',
+      location: 'Turnhout',
+      yearsExperience: 5,
+      referredBy: 'Joey Comhaire',
+      tags: ['Angular', 'React', 'Javascript'],
+    };
+  }
 
   toggleFavorite(): void {
     this.favorite = !this.favorite;
+  }
+
+  addTag(): void {
+    this.profile.tags.push(this.newTag);
+    this.newTag = '';
+  }
+
+  removeTag(tagToRemove: string): void {
+    this.profile.tags = this.profile.tags.filter((tag) => tag !== tagToRemove);
   }
 }
